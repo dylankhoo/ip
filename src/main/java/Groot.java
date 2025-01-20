62070883a582406a88c5cdc4bd5d3123508453cc
Dylan Khoo <dylankhoo@gmail.com>
2025-01-20 19:43:01 +0800

    Rename to Groot

---
+++ src/main/java/Groot.java
@@ -0,0 +1,10 @@
+public class Groot {
+    public static void main(String[] args) {
+        String logo ="  ____ ____   ___   ___ _____\n" 
+               + " / ___|  _ \\ / _ \\ / _ \\_   _|     /\\\n"
+               + "| |  _| |_) | | | | | | || |      /  \\\n"  
+               + "| |_| |  _ <| |_| | |_| || |     /____\\\n"  
+               + " \\____|_| \\_\\\\___/ \\___/ |_|       ||\n";  
+        System.out.println("I am\n" + logo);
+    }
+}  
