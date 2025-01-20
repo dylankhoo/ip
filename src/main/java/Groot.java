9ce4b2b26fcc090579327a846f6845c9af0fbddd
Dylan Khoo <dylankhoo@gmail.com>
2025-01-20 20:11:35 +0800

    Add Greet and Exit

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -1,4 +1,14 @@
 public class Groot {
+    public static void greet(){
+        System.out.println("I am Groot");
+        System.out.println("Hello! How may I help?\n");
+    }
+
+    public static void exit(){
+        System.out.println("I am Groot");
+        System.out.println("Goodbye! See you again soon.\n");
+    }
+
     public static void main(String[] args) {
         String logo ="  ____ ____   ___   ___ _____\n" 
                + " / ___|  _ \\ / _ \\ / _ \\_   _|     /\\\n"
@@ -6,5 +16,7 @@ public class Groot {
                + "| |_| |  _ <| |_| | |_| || |     /____\\\n"  
                + " \\____|_| \\_\\\\___/ \\___/ |_|       ||\n";  
         System.out.println("I am\n" + logo);
+        greet();
+        exit();
     }
 }  
