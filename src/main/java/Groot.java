7f028ccff482ba82ba82b4144a9969cf09ba9ac4
unknown <dylankhoo@gmail.com>
2025-01-25 17:39:14 +0800

    Refactor greet and bye
    - Add "say" method to support future Groot messages
    - Refactor greet and bye to use say

--- src/main/java/Groot.java
+++ src/main/java/Groot.java
@@ -2,15 +2,18 @@ public class Groot {
 
     public static String GREEN = "\u001B[32m";
     public static String RESET = "\033[0m";
+    
+    public static void say(String text){
+        System.out.println("I am Groot");
+        System.out.println(GREEN + text + "\n" + RESET);
+    }
 
     public static void greet(){
-        System.out.println("I am Groot");
-        System.out.println(GREEN + "Hello! How may I help?\n" + RESET);
+        say("Hello! How may I help?\n");
     }
 
     public static void exit(){
-        System.out.println("I am Groot");
-        System.out.println(GREEN + "Goodbye! See you again soon.\n" + RESET);
+        say("Goodbye! See you again soon.");
     }
 
     public static void main(String[] args) {
